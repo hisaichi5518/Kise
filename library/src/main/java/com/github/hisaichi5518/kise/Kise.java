f1056dedab222aaeac87578d6e065d4f9a6e5097
hisaichi5518 <hisaichi5518@gmail.com>
2017-06-23 18:38:14 +0900

    Add Kise.fetch()

--- library/src/main/java/com/github/hisaichi5518/kise/Kise.java
+++ library/src/main/java/com/github/hisaichi5518/kise/Kise.java
@@ -1,4 +1,27 @@
 package com.github.hisaichi5518.kise;
 
+import static com.github.hisaichi5518.kise.Kise.Store.INSTANCE;
+
 public class Kise {
+    enum Store {
+        INSTANCE;
+
+        Fetcher fetcher = new Fetcher();
+    }
+
+    public static void fetch() {
+        INSTANCE.fetcher.fetch();
+    }
+
+    public static void enableUrgentUpdateFlag() {
+        INSTANCE.fetcher.enableUrgentUpdateFlag();
+    }
+
+    public void disableUrgentUpdateFlag() {
+        INSTANCE.fetcher.disableUrgentUpdateFlag();
+    }
+
+    public void setCacheExpiration(long cacheExpiration) {
+        INSTANCE.fetcher.setCacheExpiration(cacheExpiration);
+    }
 }
