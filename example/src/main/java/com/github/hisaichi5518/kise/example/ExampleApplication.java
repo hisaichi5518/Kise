b0cacfd5c2363699d6e754f6369849ea090fc13c
hisaichi5518 <hisaichi5518@gmail.com>
2017-06-23 11:13:03 +0900

    Add ExampleApplication class to example app

---
+++ example/src/main/java/com/github/hisaichi5518/kise/example/ExampleApplication.java
@@ -0,0 +1,10 @@
+package com.github.hisaichi5518.kise.example;
+
+import android.app.Application;
+
+public class ExampleApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
