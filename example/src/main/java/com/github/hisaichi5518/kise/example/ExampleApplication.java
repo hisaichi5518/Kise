4007c8dec936d0a28c6120f685442a2e05fa5b25
hisaichi5518 <hisaichi5518@gmail.com>
2017-06-23 11:22:01 +0900

    initialize Kise at ExampleApplication class

--- example/src/main/java/com/github/hisaichi5518/kise/example/ExampleApplication.java
+++ example/src/main/java/com/github/hisaichi5518/kise/example/ExampleApplication.java
@@ -2,9 +2,20 @@ package com.github.hisaichi5518.kise.example;
 
 import android.app.Application;
 
+import com.github.hisaichi5518.kise.Kise;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
 public class ExampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        FirebaseApp.initializeApp(this);
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build();
+
+        Kise.initialize(configSettings, R.xml.kise);
     }
 }
