f2e76e014ef277b57e1e9d722c91ec5dffc4e172
hisaichi5518 <hisaichi5518@gmail.com>
2017-06-23 10:24:24 +0900

    Move invoke method to Unit class from Kise class.

--- library/src/main/java/com/github/hisaichi5518/kise/Kise.java
+++ library/src/main/java/com/github/hisaichi5518/kise/Kise.java
@@ -1,6 +1,4 @@
 package com.github.hisaichi5518.kise;
 
 public class Kise {
-    public static <T> void invoke(Unit<T> unit) {
-    }
 }
--- library/src/main/java/com/github/hisaichi5518/kise/Unit.java
+++ library/src/main/java/com/github/hisaichi5518/kise/Unit.java
@@ -2,6 +2,10 @@ package com.github.hisaichi5518.kise;
 
 
 public abstract class Unit<T> implements UnitActions<T> {
+    public void invoke(T actionParam) {
+
+    }
+
     String key() {
         return this.getClass().getSimpleName();
     }
