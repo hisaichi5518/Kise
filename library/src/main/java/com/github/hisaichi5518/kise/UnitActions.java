3dd3b81a29b1afa9b16dcfb0d05d4f7b16b5b85d
hisaichi5518 <hisaichi5518@gmail.com>
2017-06-23 10:12:12 +0900

    Remove Nullable annotation from UnitActions's methods

--- library/src/main/java/com/github/hisaichi5518/kise/UnitActions.java
+++ library/src/main/java/com/github/hisaichi5518/kise/UnitActions.java
@@ -1,7 +1,7 @@
 package com.github.hisaichi5518.kise;
 
 public interface UnitActions<ACTION_PARAM> {
-    void customAction(@android.support.annotation.Nullable ACTION_PARAM actionParam);
+    void customAction(ACTION_PARAM actionParam);
 
-    void defaultAction(@android.support.annotation.Nullable ACTION_PARAM actionParam);
+    void defaultAction(ACTION_PARAM actionParam);
 }
