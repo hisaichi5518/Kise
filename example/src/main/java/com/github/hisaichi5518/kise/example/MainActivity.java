6899ba3a63749bf836251e4bc410653a15cd4cf4
hisaichi5518 <hisaichi5518@gmail.com>
2017-06-23 18:39:14 +0900

    use Kise.fetch()

--- example/src/main/java/com/github/hisaichi5518/kise/example/MainActivity.java
+++ example/src/main/java/com/github/hisaichi5518/kise/example/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.github.hisaichi5518.kise.Kise;
 import com.github.hisaichi5518.kise.example.units.ViewVisibleUnit;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Kise.fetch();
+
         setContentView(R.layout.activity_main);
 
         View view = findViewById(R.id.activity_main__text);
