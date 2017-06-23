3f2009b0790f4970e090b43ecbcad204bc0857e3
hisaichi5518 <hisaichi5518@gmail.com>
2017-06-23 18:38:48 +0900

    Remove fetch logic from invoke method.

--- library/src/main/java/com/github/hisaichi5518/kise/Unit.java
+++ library/src/main/java/com/github/hisaichi5518/kise/Unit.java
@@ -1,47 +1,21 @@
 package com.github.hisaichi5518.kise;
 
 
-import android.support.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 
 public abstract class Unit<ActionParam> implements UnitActions<ActionParam> {
-    private long cacheExpiration = 43200L; // 12 hours
 
     public void invoke(final ActionParam actionParam) {
         final FirebaseRemoteConfig config = FirebaseRemoteConfig.getInstance();
-        config.fetch(cacheExpiration)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        config.activateFetched();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Ignore Exception
-                    }
-                })
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (config.getBoolean(key())) {
-                            try {
-                                customAction(actionParam);
-                            } catch (Exception e) {
-                                defaultAction(actionParam);
-                            }
-                        } else {
-                            defaultAction(actionParam);
-                        }
-                    }
-                });
-
+        if (config.getBoolean(key())) {
+            try {
+                customAction(actionParam);
+            } catch (Exception e) {
+                defaultAction(actionParam);
+            }
+        } else {
+            defaultAction(actionParam);
+        }
         // TODO: Callback methods does not execute if Google play service is out date.
     }
 
