bc964f282bc1896fab189927eca82b81cf5d7363
hisaichi5518 <hisaichi5518@gmail.com>
2017-06-24 15:24:44 +0900

    Add UnitWithReturn class

---
+++ library/src/main/java/com/github/hisaichi5518/kise/unittype/UnitWithReturn.java
@@ -0,0 +1,27 @@
+package com.github.hisaichi5518.kise.unittype;
+
+
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+
+public abstract class UnitWithReturn<ReturnValue> {
+    protected abstract ReturnValue customAction() throws Exception;
+
+    protected abstract ReturnValue defaultAction();
+
+    public ReturnValue invoke() {
+        final FirebaseRemoteConfig config = FirebaseRemoteConfig.getInstance();
+        if (config.getBoolean(key())) {
+            try {
+                return customAction();
+            } catch (Exception e) {
+                return defaultAction();
+            }
+        } else {
+            return defaultAction();
+        }
+    }
+
+    private String key() {
+        return this.getClass().getSimpleName();
+    }
+}
